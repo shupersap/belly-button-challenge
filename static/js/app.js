21695c942ffaf33e4f31d8e990f406afb97a3c8c
shupersap <shupersap@gmail.com>
2023-03-03 22:54:00 -0500

    updated

--- static/js/app.js
+++ static/js/app.js
@@ -1,93 +1,70 @@
-const url = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json"
-d3.json(url).then(data=>{
-    console.log(data)
-})
+const dataUrl = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json";
 
+let data;
 
-function main() {
-    let dropdownMenu = d3.select("#selDataset");
-    d3.json(url).then((data) => {
-      let Test_ID = data.names;
-      Test_ID.forEach((subject) => {
-        dropdownMenu
-          .append("option")
-          .text(subject)
-          .property("value", subject);
-      });
-    
-      demographicInfo();
-      createCharts();
-    });
-    }
+const getData = async () => {
+  data = await d3.json(dataUrl);
+};
 
-function demographicInfo(subject){
-d3.json(url).then(data => {
-let metaData = data.metadata
-let resultsArray = metaData.filter(sampleobject => sampleobject.id == subject)
-console.log(resultsArray)
-let result = resultsArray[0]
-let demographicPanel = d3.select("#sample-metadata")
-demographicPanel.html("")
-Object.entries(result).forEach(([key, value]) => {
-    demographicPanel.append("h6").text(`${key}: ${value}`);
-})
-})
+const createDropdown = () => {
+  const dropdownMenu = d3.select("#selDataset");
+  const { names } = data;
+  names.forEach((id) => dropdownMenu.append("option").text(id).property("value", id));
+};
 
-}
+const createDemographicInfo = (subject) => {
+  const { metadata } = data;
+  const result = metadata.find(({ id }) => id == subject);
+  const demographicPanel = d3.select("#sample-metadata");
+  demographicPanel.html("");
+  Object.entries(result).forEach(([key, value]) => demographicPanel.append("h6").text(`${key}: ${value}`));
+};
 
-  function createCharts(subject) {
-  
-  d3.json(url).then((data) => {
-    let samples= data.samples;
-    let values= samples.filter(sampleobject => sampleobject.id == subject);
-    let value= values[0]
-    let ids = value.otu_ids;
-    let labels = value.otu_labels;
-    let result = value.sample_values;
+const createCharts = (subject) => {
+  const { samples } = data;
+  const { sample_values, otu_ids, otu_labels } = samples.find(({ id }) => id == subject);
 
-  let barData =[
-    {
-      x:result.slice(0,10).reverse(),
-      y:ids.slice(0, 10).map(otuID => `OTU ${otuID}`).reverse(),
-      text:labels.slice(0,10).reverse(),
-      type:"bar",
-      orientation:"h"
-
-    }
-  ];
+  const barData = [{
+    x: sample_values.slice(0, 10).reverse(),
+    y: otu_ids.slice(0, 10).map((id) => `OTU ${id}`).reverse(),
+    text: otu_labels.slice(0, 10).reverse(),
+    type: "bar",
+    orientation: "h",
+  }];
 
   Plotly.newPlot("bar", barData);
 
-      let bubbleData = [ 
-      {
-        x: ids,
-        y: result,
-        text: labels,
-        mode: "markers",
-        marker: {
-          color: ids,
-          size: result,
-          }
-      }
-    ];
-  
-    let bubbleLayout = {
-        xaxis: { title: "OTU ID" },
-        hovermode: "closest",
-        };
-    
-    
-    Plotly.newPlot("bubble", bubbleData, bubbleLayout);
-  
-  
-  });
-  }
-   
-  function optionChanged(newSample) {
+  const bubbleData = [{
+    x: otu_ids,
+    y: sample_values,
+    text: otu_labels,
+    mode: "markers",
+    marker: {
+      color: otu_ids,
+      size: sample_values,
+    },
+  }];
 
-    createCharts(newSample);
-    demographicInfo(newSample);
-  }
-  
+  const bubbleLayout = {
+    xaxis: { title: "OTU ID" },
+    hovermode: "closest",
+  };
 
-  main();
+  Plotly.newPlot("bubble", bubbleData, bubbleLayout);
+};
+
+const optionChanged = (newSample) => {
+  createCharts(newSample);
+  createDemographicInfo(newSample);
+};
+
+const main = () => {
+  return getData()
+    .then(() => {
+      createDropdown();
+      createDemographicInfo(data.names[0]);
+      createCharts(data.names[0]);
+    });
+};
+
+main();
